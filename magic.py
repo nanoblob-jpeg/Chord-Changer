ca5d6f7d726cf8590d709436f04167d21c6e59c1
nanoblob-jpeg <jerry.li1248@gmail.com>
2020-04-11 14:27:48 -0700

    added suport for inversions

--- magic.py
+++ magic.py
@@ -35,49 +35,49 @@ def toGreek(i, scale):
 scale = input("what scale is this song in? Ex: A or A# or Ab or Am or Abm or A#m: ");
 letters = ["A", "B", "C", "D", "E", "F", "G"];
 scales = {
-	"Ab" : ("Ab", "Bbm", "Cm", "Db", "Eb", "Fm", "G"),
-	"A" : ("A", "Bm", "C#m", "D", "E", "F#m", "G#"),
-	"A#" : ("A#", "B#m", "C##m", "D#", "E#", "F##m", "G##"),
-	"Bb" : ("Bb", "Cm", "Dm", "Eb", "F", "Gm", "A"),
-	"B" : ("B", "C#m", "D#m", "E", "F#", "G#m", "A"),
-	"B#" : ("B#", "C##m", "D##m", "E#", "F##", "G##m", "A##"),
-	"Cb" : ("Cb", "Dbm", "Ebm", "Fb", "Gb", "Abm", "Bb"),
-	"C" : ("C", "Dm", "Em", "F", "G", "Am", "B"),
-	"C#" : ("C#", "D#m", "E#m", "F#", "G#", "A#m","B#"),
-	"Db" : ("Db", "Ebm", "Fm", "Gb", "Ab", "Bbm", "C"),
-	"D" : ("D", "Em", "F#m", "G", "A", "Bm", "C#"),
-	"D#" : ("D#", "E#m", "F##m", "G#", "A#", "B#m", "C##"),
-	"Eb" : ("Eb", "Fm", "Gm", "Ab", "Bb", "Cm", "D"),
-	"E" : ("E", "F#m", "G#m", "A", "B", "C#m", "D#"),
-	"E#" : ("E#", "F##m", "G##m", "A#", "B#", "C##m", "D##"),
-	"Fb" : ("Fb", "Gbm", "Abm", "Bbb", "Cb", "Dbm", "Eb"),
-	"F" : ("F", "Gm", "Am", "Bb", "C", "Dm", "E"),
-	"F#" : ("F#", "G#m", "A#m", "B", "C#", "D#m", "E#"),
-	"Gb" : ("Gb", "Abm", "Bbm", "Cb", "Db", "Ebm", "F"),
-	"G" : ("G", "Am", "Bm", "C", "D", "Em", "F#"),
-	"G#" : ("G#", "A#m", "B#m", "C#", "D#", "E#m", "F##"),
+	"Ab" : ("Ab", "Bb", "C", "Db", "Eb", "F", "G"),
+	"A" : ("A", "B", "C#", "D", "E", "F#", "G#"),
+	"A#" : ("A#", "B#", "C##", "D#", "E#", "F##", "G##"),
+	"Bb" : ("Bb", "C", "D", "Eb", "F", "G", "A"),
+	"B" : ("B", "C#", "D#", "E", "F#", "G#", "A"),
+	"B#" : ("B#", "C##", "D##", "E#", "F##", "G##", "A##"),
+	"Cb" : ("Cb", "Db", "Eb", "Fb", "Gb", "Ab", "Bb"),
+	"C" : ("C", "D", "E", "F", "G", "A", "B"),
+	"C#" : ("C#", "D#", "E#", "F#", "G#", "A#","B#"),
+	"Db" : ("Db", "Eb", "F", "Gb", "Ab", "Bb", "C"),
+	"D" : ("D", "E", "F#", "G", "A", "Bm", "C#"),
+	"D#" : ("D#", "E#", "F##", "G#", "A#", "B#", "C##"),
+	"Eb" : ("Eb", "F", "G", "Ab", "Bb", "C", "D"),
+	"E" : ("E", "F#", "G#", "A", "B", "C#", "D#"),
+	"E#" : ("E#", "F##", "G##", "A#", "B#", "C##", "D##"),
+	"Fb" : ("Fb", "Gb", "Ab", "Bbb", "Cb", "Db", "Eb"),
+	"F" : ("F", "G", "A", "Bb", "C", "D", "E"),
+	"F#" : ("F#", "G#", "A#", "B", "C#", "D#", "E#"),
+	"Gb" : ("Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F"),
+	"G" : ("G", "A", "B", "C", "D", "E", "F#"),
+	"G#" : ("G#", "A#", "B#", "C#", "D#", "E#", "F##"),
 
-	"Abm" : ("Abm", "Bbm", "Cb", "Dbm", "Eb", "Fb", "G"),
-	"Am" : ("Am", "Bm", "C", "Dm", "E", "F", "G#"),
-	"A#m" : ("A#m", "B#m", "C#", "D#m", "E#", "F#", "G##"),
-	"Bbm" : ("Bbm", "Cm", "Db", "Ebm", "F", "Gb", "A"),
-	"Bm" : ("Bm", "C#m", "D", "Em", "F#", "G", "A#"),
-	"B#m" : ("B#m", "C##m", "D#", "E#m", "F##", "G#", "A##"),
-	"Cbm" : ("Cbm", "Dbm", "Ebb", "Fbm", "Gb", "Abb", "Bb"),
-	"Cm" : ("Cm", "Dm", "Eb", "Fm", "G", "Ab", "B"),
-	"C#m" : ("C#m", "D#m", "E", "F#m", "G#", "A","B#"),
-	"Dbm" : ("Dbm", "Ebm", "Fb", "Gbm", "Ab", "Bbb", "C"),
-	"Dm" : ("Dm", "Em", "F", "Gm", "A", "Bb", "C#"),
-	"D#m" : ("D#m", "E#m", "F#", "G#m", "A#", "B", "C##"),
-	"Ebm" : ("Ebm", "Fm", "Gb", "Abm", "Bb", "Cb", "D"),
-	"Em" : ("Em", "F#m", "G", "Am", "B", "C", "D#"),
-	"E#m" : ("E#m", "F##m", "G#", "A#m", "B#", "C#", "D##"),
-	"Fbm" : ("Fbm", "Gbm", "Abb", "Bbbm", "Cb", "Dbb", "Eb"),
-	"Fm" : ("Fm", "Gm", "Ab", "Bbm", "C", "Db", "E"),
-	"F#m" : ("F#m", "G#m", "A", "Bm", "C#", "D", "E#"),
-	"Gbm" : ("Gbm", "Abm", "Bbb", "Cbm", "Db", "Ebb", "F"),
-	"Gm" : ("Gm", "Am", "Bb", "Cm", "D", "Eb", "F#"),
-	"G#m" : ("G#m", "A#m", "B", "C#m", "D#", "E", "F##")
+	"Abm" : ("Ab", "Bb", "Cb", "Db", "Eb", "Fb", "G"),
+	"Am" : ("A", "B", "C", "Dm", "E", "F", "G#"),
+	"A#m" : ("A#", "B#", "C#", "D#", "E#", "F#", "G##"),
+	"Bbm" : ("Bb", "C", "Db", "Eb", "F", "Gb", "A"),
+	"Bm" : ("B", "C#", "D", "E", "F#", "G", "A#"),
+	"B#m" : ("B#", "C##", "D#", "E#", "F##", "G#", "A##"),
+	"Cbm" : ("Cb", "Db", "Ebb", "Fb", "Gb", "Abb", "Bb"),
+	"Cm" : ("C", "D", "Eb", "F", "G", "Ab", "B"),
+	"C#m" : ("C#", "D#", "E", "F#", "G#", "A","B#"),
+	"Dbm" : ("Db", "Eb", "Fb", "Gb", "Ab", "Bbb", "C"),
+	"Dm" : ("D", "E", "F", "G", "A", "Bb", "C#"),
+	"D#m" : ("D#", "E#", "F#", "G#", "A#", "B", "C##"),
+	"Ebm" : ("Eb", "F", "Gb", "Ab", "Bb", "Cb", "D"),
+	"Em" : ("E", "F#", "G", "A", "B", "C", "D#"),
+	"E#m" : ("E#", "F##", "G#", "A#", "B#", "C#", "D##"),
+	"Fbm" : ("Fb", "Gb", "Abb", "Bbb", "Cb", "Dbb", "Eb"),
+	"Fm" : ("F", "G", "Ab", "Bb", "C", "Db", "E"),
+	"F#m" : ("F#", "G#", "A", "B", "C#", "D", "E#"),
+	"Gbm" : ("Gb", "Ab", "Bbb", "Cb", "Db", "Ebb", "F"),
+	"Gm" : ("G", "A", "Bb", "C", "D", "Eb", "F#"),
+	"G#m" : ("G#", "A#", "B", "C#", "D#", "E", "F##")
 }
 
 if(scale not in scales.keys()):
@@ -96,8 +96,10 @@ while True:
 
 for i in range(len(contents)):
 	if contents[i].count(" ") > len(contents[i])/4 +1:
+		contents[i] = contents[i].replace("m", " ");
 		for j in scales[scale]:
 			contents[i] = contents[i].replace(j, toGreek(scales[scale].index(j) + 1, scale))
+
 	f.write(contents[i]);
 	f.write("\n")
 
